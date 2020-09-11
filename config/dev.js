9ce32325dafadb448cc7a60009f8312247654b19
nutchapon.h <nutchapon@enres.com>
2020-09-11 14:56:02 +0700

    blog app - add redis server cached layer

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb://admin:rSfopbzUVJXqCi7y@blog-app-node-js-shard-00-00.ww1ro.mongodb.net:27017,blog-app-node-js-shard-00-01.ww1ro.mongodb.net:27017,blog-app-node-js-shard-00-02.ww1ro.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-rh0sbo-shard-0&authSource=admin&retryWrites=true&w=majority",
+  cookieKey: "123123123",
 };
---
+++ routes/Person..js
@@ -0,0 +1,11 @@
+Person.find({ occupation: /host/ })
+  .where("name.last")
+  .equals("Ghost")
+  .where("age")
+  .gt(17)
+  .lt(66)
+  .where("likes")
+  .in(["vaporizing", "talking"])
+  .limit(10)
+  .sort("-occupation")
+  .select("name occupation");
