79bc80490bea59f7da2f58b19c0699a355c3ae26
nutchapon.h <nutchapon@enres.com>
2020-09-13 22:27:10 +0700

    blog app - add travisCI yaml config file

--- config/dev.js
+++ config/dev.js
@@ -5,4 +5,5 @@ module.exports = {
   mongoURI:
     "mongodb://admin:rSfopbzUVJXqCi7y@blog-app-node-js-shard-00-00.ww1ro.mongodb.net:27017,blog-app-node-js-shard-00-01.ww1ro.mongodb.net:27017,blog-app-node-js-shard-00-02.ww1ro.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-rh0sbo-shard-0&authSource=admin&retryWrites=true&w=majority",
   cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
 };
